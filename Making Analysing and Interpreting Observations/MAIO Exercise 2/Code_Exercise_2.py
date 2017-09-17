d570efc7d5e209d94bf65b24ac232f21155e8b4b
WimUU <w.e.vancaspel@students.uu.nl>
2017-09-17 17:40:54 +0200

    Summary

---
+++ Making Analysing and Interpreting Observations/MAIO Exercise 2/Code_Exercise_2.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 11:54:00 2017
+
+@author: Wim
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+import random
+from scipy.fftpack import fft, fftfreq, fftshift
+
+#%% SECTION B: Fourier transform algorithm
+    
+def fourier_transform(x):
+    # INPUT: Array (1-Dimensional)
+    # OUTPUT: Non-scaled array with spectral analysis of input (time) series
+    x = np.asarray(x, dtype=float)  # convert to array (computationally beneficial)
+    N = len(x)
+    n = np.arange(N)                # harmonics
+    k = n.reshape((N, 1))           # wave-number
+    M = np.exp(-2j*np.pi*k*n/N)     # for each k evaluate for all n
+    return np.dot(M, x) 
+           
+#%% SECTION A: functions for initializing the desired time series.
+
+def unit_impulse(spacing, length,make_plot=False):
+    # INPUT: 'spacing' is kyrs between time steps
+    # length (in kyrs) of data set
+    x = np.zeros(int(length/spacing))
+    x[len(x)//2] = 1
+    
+    if make_plot:
+        n = len(x)
+        yf = fourier_transform(x)
+        xf = np.linspace(0.0,1.0/(2.0*spacing),n//2)
+        plt.figure()
+        plt.plot(xf,1.0/n*abs(yf[0:n//2]))
+        plt.xlim((0,0.05))
+        plt.title('Frequency domain',fontsize=15)
+        plt.ylabel('DFT Value',fontsize=15), plt.xlabel('Frequency (per kyrs)',fontsize=15)
+        plt.grid()
+        plt.show()
+        
+    return x
+
+def sine_wave(period,length,spacing, make_plot=False,int_pwr=False):
+    #INPUT: period (kyrs), length (kyrs), (sample) spacing (kyrs)
+    x = np.arange(length)
+    x = np.sin(2*np.pi*1/period*x)
+
+    if make_plot:
+        n = len(x)
+        yf = fourier_transform(x)
+        xf = np.linspace(0.0,1/(2.0*spacing),n//2)
+        if int_pwr:
+            yf = abs(yf)**2
+        plt.figure()
+        plt.plot(xf,1.0/n*abs(yf[0:n//2]))
+        plt.xlim((0,0.1))
+        plt.title('Frequency domain',fontsize=15)
+        plt.ylabel('DFT Value',fontsize=15), plt.xlabel('Frequency (per kyrs)',fontsize=15)
+        if int_pwr:
+             plt.title('Integrated power freq. domain',fontsize=15)
+             plt.ylabel('|DFT Value|$^2$',fontsize=15)  
+        plt.grid()
+        plt.show()
+        
+    return x
+
+def sawtooth(period,length,spacing,noise=False,variations=False,make_plot=False,int_pwr=False):
+    #INPUT: period (average, kyrs) end_time length data set (yrs)
+    # 'noise = True' introduces gaussian noise. 
+    # 'variations=True' introduces +- 0.02myr variations in sawtooth periods
+    # OUTPUT: Sawtooth function with amplitude 1
+
+    x = []                          # empty list to which data is appended
+    a = 0                           # counter for keeping track of variations
+    b = [0,0,0,0,0,0,0,0,0,0]       # if 'variations=False'
+    
+    if variations:
+        b = [20,20,20,20,20,-20,-20,-20,-20,-20]
+        random.shuffle(b)           # randomly shuffle +- 0.02myr variations
+    
+    for i in range(int(length/period)):
+        y = 1/(period+b[a])      # slope of sawtooth 
+        for j in range(100+b[a]):   # variation-adjusted period length
+            x.append(y*j) 
+        a += 1
+        
+    if noise:                       # add noise to each data point
+        for i in range(len(x)):     # adjust scale to increase nose amplitude
+            x[i] = x[i] + np.random.normal(scale=10.0)
+            
+    if make_plot:
+        n = len(x)
+        yf = fourier_transform(x)
+        xf = np.linspace(0.0,1.0/(2.0*spacing),n//2)
+        if int_pwr:
+            yf = abs(yf)**2
+        plt.figure()
+        plt.plot(xf,1.0/n*abs(yf[0:n//2]))
+        plt.title('Frequency domain',fontsize=15)
+        plt.ylabel('DFT Value',fontsize=15), plt.xlabel('Frequency (per kyrs)',fontsize=15)
+        if int_pwr:
+             plt.title('Integrated power freq. domain',fontsize=15)
+             plt.xlim((0.001,0.1))             
+             plt.ylim(((0,30)))
+             plt.ylabel('|DFT Value|$^2$',fontsize=15)  
+        plt.grid()
+        plt.show()
+        
+    return x
+
+def signal_plotter(s):
+    x = np.arange(len(s))
+    plt.figure()
+    plt.plot(x,s)
+    plt.title('Time domain',fontsize=15)
+    plt.ylabel('f(t)',fontsize=15), plt.xlabel('t (kyrs)',fontsize=15)
+    plt.grid()
+    plt.show()
+    
+#%% SECTION C: Fourier Transforms of sections A's time series.
+    
+signal_plot = True # Set 'signal_plot = True' for time-domain plots
+
+# Question 1. 0.6 Myr time-series (600 kyrs). impulse halfway at 300 kyrs.
+
+impulse = unit_impulse(1,600,make_plot=False)
+
+# Question 2: 1000 kyrs sine-wave with wavelength 100 kyrs, amplitude 1
+
+sinwav = sine_wave(100,1000,1,make_plot=False,int_pwr=True)
+
+# Question 3: 1000 kyrs sawtooth function with wavelength 100 kyrs, amplitude 1
+
+saw = sawtooth(100,1000,1,make_plot=False,int_pwr=True)
+
+# Question 5: 1000 kyrs sawtooth time series with randomly 20 kyrs variations 
+# in period. Amplitude 1. 
+
+saw_var = sawtooth(100,1000,1,variations=True,make_plot=False)
+
+# Question 6: Repeat question 4 but with significant noise added to ampltiude
+
+saw_var2 = sawtooth(100,1000,1,noise=True,variations=True,make_plot=True)
+
+if signal_plot:
+    signal_plotter(impulse)
+    signal_plotter(sinwav)
+    signal_plotter(saw)
+    signal_plotter(saw_var)
+    signal_plotter(saw_var2)
