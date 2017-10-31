dd7d0636d406ba1a0bfba988ecb223d7281957a7
Bryan <b.brouwer.b@gmail.com>
2017-10-31 09:40:00 +0100

    nieuw
    nieuw

--- Ocean Drifter Analysis/avisoread.py
+++ Ocean Drifter Analysis/avisoread.py
@@ -13,6 +13,12 @@ from mpl_toolkits.basemap import Basemap
 import os
 import gc
 
+folder = 'E:/Bryan/Downloads/'
+direcs = np.array([os.listdir(folder + 'AVISO')])
+
+coordinates_1 = np.load(folder + 'driftcoord/drifter_01.npy')
+coordinates_2 = np.load(folder + 'driftcoord/drifter_02.npy')
+
 y=np.random.randn(10, 720, 1440)
 
 latbounds = [ -60 , -50 ]
@@ -28,7 +34,6 @@ latui = np.argmin( np.abs( lats - latbounds[1] ) )
 lonli = np.argmin( np.abs( lons - lonbounds[0] ) )
 lonui = np.argmin( np.abs( lons - lonbounds[1] ) )  
 
-direcs = np.array([os.listdir('E:\Bryan\Downloads\AVISO')])
 fig = plt.figure()
 my_map=Basemap(llcrnrlon=lonbounds[0],llcrnrlat=latbounds[0],urcrnrlon=lonbounds[1],urcrnrlat=latbounds[1],resolution='l',projection ='merc')
 my_map.drawcoastlines()
@@ -41,6 +46,10 @@ lons, lats = np.meshgrid(lons[lonli:lonui:4],lats[latli:latui:4])
 cs = None
 q = None
 cbar = None
+x,y = my_map(0, 0)
+punt1 = my_map.plot(x, y, 'ro', markersize=5)[0]
+punt2 = my_map.plot(x, y, 'ro', markersize=5)[0]
+
 
 def init():
     return my_map                                                                                                                                                                                                                                                                                          
@@ -48,7 +57,7 @@ def init():
 def animate(i):
     global cs
     global q
-    global cbar
+#    global punt
     if cs != None:
         cs.remove()
         q.remove()
@@ -63,6 +72,10 @@ def animate(i):
     cbar.set_label('$[ms^{-1}]$')
     plt.clim(0,1.5)
     q = my_map.quiver(lons,lats,us,vs, scale =15, latlon=True)
+    x,y = my_map(coordinates_1[i,0],coordinates_1[i,1])
+    punt1.set_data(x,y)
+    x2,y2 = my_map(coordinates_2[i,0],coordinates_2[i,1])
+    punt2.set_data(x2,y2)
     gc.collect()
     print(i)
     return my_map
@@ -70,9 +83,11 @@ cs = None
 q= None
 cbar = None
 
+
 #mywriter = animation.FFMpegWriter(codec="libx264",bitrate=-1)
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=234, interval=100,save_count=0, repeat=False) #interval = number of milliseconds between frames
-anim.save('movie.mp4')
+# kies aantal frames niet langer dan 232!
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=40, interval=100,save_count=0, repeat=False) #interval = number of milliseconds between frames
+anim.save('movie.mp4', bitrate=500)
 
 #aviso = ncdf.Dataset("dt_global_twosat_madt_uv_20131211_20140704.nc", mode = 'r')
 ##lats = aviso.variables['lat'][:]
@@ -111,7 +126,8 @@ anim.save('movie.mp4')
 #u = aviso.variables['u'][0,:,:]
 #v = aviso.variables['v'][0,:,:]
 #N = np.sqrt(u**2+v**2)
-#my_map.quiver(lons,lats,u,v,scale=20,latlon=True)
+#x,y = my_map(160,-55)
+#my_map.plot(x,y,marker='D', color = 'm')
 #plt.show()
 
 #cs = my_map.pcolormesh(lons,lats,u,cmap='seismic', shading='flat',latlon=True)
